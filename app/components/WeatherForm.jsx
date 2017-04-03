d119c8eaf75d3a64303480d97fe6e9323087ca02
anmol678 <anmolj678@gmail.com>
2017-04-02 19:15:29 -0700

    Added CSS

--- app/components/WeatherForm.jsx
+++ app/components/WeatherForm.jsx
@@ -14,7 +14,7 @@ var WeatherForm = React.createClass({
   render: function () {
     return (
         <form onSubmit={this.onFormSubmit}>
-          <input type="text" ref="location" placeholder="Enter city name"/>
+          <input type="search" ref="location" placeholder="Enter location"/>
           <button className="hollow button expanded">Get Weather</button>
         </form>
     );
